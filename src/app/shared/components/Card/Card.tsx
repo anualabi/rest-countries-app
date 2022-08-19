1aaa9d4466fe1922550c3b4051c94401f755db3a
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2022-08-19 15:52:48 +0200

    add background color to card component

--- src/app/shared/components/Card/Card.tsx
+++ src/app/shared/components/Card/Card.tsx
@@ -9,6 +9,7 @@ const Card = ({ children }: Children) => {
 
 const StyledCard = styled.div`
   ${css`
+    background-color: ${(props) => props.theme.element};
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
     transition: 0.3s;
 
