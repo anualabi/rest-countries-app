8472b18669af63138fad07301c625c1e7f3fcae5
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2022-08-24 01:53:10 +0200

    ButtonComponent - update icon class names

--- src/app/shared/components/Button/Button.tsx
+++ src/app/shared/components/Button/Button.tsx
@@ -12,9 +12,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, startIcon, endIcon, ...props }, ref) => {
     return (
       <button className={`${styles.button} ${className}`} ref={ref} type="button" {...props}>
-        {startIcon && <span className="material-icons start-icon">{startIcon}</span>}
+        {startIcon && <span className={'material-symbols-outlined start-icon'}>{startIcon}</span>}
         {children}
-        {endIcon && <span className="material-icons end-icon">{endIcon}</span>}
+        {endIcon && <span className="material-symbols-outlined end-icon">{endIcon}</span>}
       </button>
     );
   }
