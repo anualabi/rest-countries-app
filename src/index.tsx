514b17b68a7c142ff0a69477705e8ffccc3a47c6
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2022-09-13 01:14:19 +0200

    add css normalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Normalize } from 'styled-normalize';
 import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Normalize />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
